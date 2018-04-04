38fb217bb0f548f0677a21143a8ae5fb8b0d2918
ctabis <ctabis@venacorp.com>
2018-04-04 18:14:34 -0400

    add link to google search of allegations

--- popup.js
+++ popup.js
@@ -1,11 +1,32 @@
 'use strict';
 
-var listOfTherapists = ["Jeffrey Tambor", "Casey Affleck"];
+var listOfTherapists = [
+"Jeffrey Tambor",
+"Casey Affleck",
+"Weinstein",
+"Kevin Spacey",
+"Russell Simmons",
+"Dustin Hoffman",
+"James Toback",
+"Matt Lauer",
+"Charlie Rose",
+"Brett Ratner",
+"Tom Sizemore",
+"Steven Seagal",
+"Louis C.K.",
+"Danny Masterson",
+"T.J. Miller",
+"Jeremy Piven",
+"Ed Westwick",
+"Paul Haggis",
+"Mark Schwahn",
+"Gary Goddard",
+"Robert Knepper"];
 
 var pageContent;
+var alreadyScanned = false;
 
-
-changeColor.onclick = function(element) {
+scanButton.onclick = function(element) {
 	  
 	 function modifyDOM() {
         //You can play with your DOM here or check URL against your regex
@@ -13,18 +34,22 @@ changeColor.onclick = function(element) {
         console.log(document.body);
         return document.body.innerHTML;
     }
-
-    //We have permission to access the activeTab, so we can call chrome.tabs.executeScript:
-    chrome.tabs.executeScript({
-        code: '(' + modifyDOM + ')();' //argument here is a string but function.toString() returns function's code
-    }, (results) => {
-        //Here we have just the innerHTML and not DOM structure
-        console.log('Popup script:')
-        console.log(results[0]);
-		pageContent = results[0];
-		// Search through page and add list of problematic people	  
-		scanForTherapists();
-    });   
+	
+	if (!alreadyScanned){
+	
+		//We have permission to access the activeTab, so we can call chrome.tabs.executeScript:
+		chrome.tabs.executeScript({
+			code: '(' + modifyDOM + ')();' //argument here is a string but function.toString() returns function's code
+		}, (results) => {
+			//Here we have just the innerHTML and not DOM structure
+			console.log('Popup script:')
+			console.log(results[0]);
+			pageContent = results[0];
+			// Search through page and add list of problematic people	  
+			scanForTherapists();
+		});   
+	alreadyScanned = true;
+	}
 };
  
 function scanForTherapists() {
@@ -35,12 +60,20 @@ function scanForTherapists() {
         }
     });  
 }
+
+function openNewTab() {
+    chrome.tabs.create({url: this.href});
+    return false;
+}
   
 function addTherapist (therapist) { 
     // create a new div element 
-    var newDiv = document.createElement("div"); 
-    var newContent = document.createTextNode(therapist); 
-    newDiv.appendChild(newContent);  
+	var link = "https://www.google.ca/search?q=sexual+assault+allegations+against+" +therapist.replace(" ","+");
+    var newDiv = document.createElement("a"); 
+	newDiv.setAttribute('href', link);
+    newDiv.innerHTML =  therapist;
+	newDiv.onclick = openNewTab;
 
     document.getElementById("listOfPeopleDiv").appendChild(newDiv);
 }
+
